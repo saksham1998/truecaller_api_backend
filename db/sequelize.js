03054f9adbbdc98f53a85324d1c4a96a53c59673
Saksham Manocha <sakshammanocha14@gmail.com>
2020-06-11 13:50:16 +0530

    Update sequelize.js

--- db/sequelize.js
+++ db/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config()
-const sequelize = new Sequelize('truecaller','root',process.env.DB_PASS,
+const sequelize = new Sequelize('your_db_name','your_db_user','your_db_password',
     {
         dialect:'mysql',
         host:'localhost'
